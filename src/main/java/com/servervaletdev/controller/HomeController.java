4ee98c74072aab5da4aa1b420ce0fc8ba1ef0302
Robert Mihai Colca <robery_office@yahoo.ro>
2018-12-26 21:09:50 +0200

    Added the dashboard module

--- src/main/java/com/servervaletdev/controller/HomeController.java
+++ src/main/java/com/servervaletdev/controller/HomeController.java
@@ -7,15 +7,14 @@ import java.util.Map;
 
 //@RestController
 @Controller
-@RequestMapping(path="/home")
 public class HomeController {
     /**
      * Dashboard Action
      * @param model the data to send to the view
      * @return template name
      */
-    @GetMapping(path= "/index")
-    public String index(Map<String, Object> model){
+    @GetMapping(path= "/home/index")
+    public String indexAction(Map<String, Object> model){
         model.put("moduleName", "dashboard");
         return "admin";
     }
