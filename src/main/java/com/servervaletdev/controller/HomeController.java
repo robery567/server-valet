3915a148f00b25ad1a19e664b629406622a30600
Robert Mihai Colca <robery_office@yahoo.ro>
2018-12-26 20:47:51 +0200

    Modularised the template

--- src/main/java/com/servervaletdev/controller/HomeController.java
+++ src/main/java/com/servervaletdev/controller/HomeController.java
@@ -3,12 +3,15 @@ package com.servervaletdev.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 //@RestController
 @Controller
 @RequestMapping(path="/home")
 public class HomeController {
     @GetMapping(path= "/index")
-    public String index(){
-        return "dashboard";
+    public String index(Map<String, Object> model){
+        model.put("moduleName", "dashboard");
+        return "account";
     }
 }
